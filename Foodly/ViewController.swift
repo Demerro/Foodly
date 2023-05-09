fd34044a0f8a85533c11b5317e5be82eb6cc0c4a
Dewerro <funcake0@gmail.com>
2023-05-09 18:27:42 +0300

    Initial Commit

---
+++ Foodly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodly
+//
+//  Created by Nikita Prokhorchuk on 9.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
