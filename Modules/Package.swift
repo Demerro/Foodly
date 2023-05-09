947656888855be72d808593a3b09c435fba404e6
Dewerro <funcake0@gmail.com>
2023-05-09 18:34:10 +0300

    Setup project

---
+++ Modules/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Modules",
+    products: [
+//        .library(
+//            name: "Modules",
+//            targets: ["Modules"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+//        .target(
+//            name: "Modules",
+//            dependencies: [])
+    ]
+)
